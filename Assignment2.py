75be84fa3a418bbb185a48043587f19c743e5ad2
sravanthiyaragani <sravanthiyara41@gmail.com>
2026-06-26 19:57:01 +0530

    python assignment 2

---
+++ Assignment2.py
@@ -0,0 +1,49 @@
+a = 10
+b = 10
+
+print("Value of a =", a)
+print("Value of b =", b)
+
+print("\n1. XOR Operation (Same Values)")
+print("a ^ b =", a ^ b)
+
+print("\nExplanation:")
+print("When two values are the same, XOR returns 0.")
+
+print("\n2. AND Operation")
+print("a & b =", a & b)
+
+print("\n3. Check Even or Odd using AND Operator")
+
+num = 10
+
+if (num & 1) == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
+
+print("\n4. Practice All Bitwise Operators")
+
+x = 12
+y = 5
+
+print("x =", x)
+print("y =", y)
+
+print("\nAND (&)")
+print("x & y =", x & y)
+
+print("\nOR (|)")
+print("x | y =", x | y)
+
+print("\nXOR (^)")
+print("x ^ y =", x ^ y)
+
+print("\nNOT (~)")
+print("~x =", ~x)
+
+print("\nLeft Shift (<<)")
+print("x << 2 =", x << 2)
+
+print("\nRight Shift (>>)")
+print("x >> 2 =", x >> 2)
