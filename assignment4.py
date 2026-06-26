124418c3994edd42ab158fb742de932c4c8c8367
sravanthiyaragani <sravanthiyara41@gmail.com>
2026-06-26 20:13:15 +0530

    python task 4

---
+++ assignment4.py
@@ -0,0 +1,26 @@
+# Input and Output Functions
+
+# 1. Taking integer input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Output
+print("First Number =", a)
+print("Second Number =", b)
+
+# Arithmetic Operations
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+
+# String Input
+name = input("Enter your name: ")
+print("Welcome", name)
+
+# Float Input
+salary = float(input("Enter your salary: "))
+print("Salary =", salary)
+
+# Boolean Output
+print("Is salary greater than 50000?", salary > 50000)
