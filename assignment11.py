21254be0dba974f5e485bf029ac51f2a22baf94a
sravanthiyaragani <sravanthiyara41@gmail.com>
2026-06-26 20:41:09 +0530

    python task 11

---
+++ assignment11.py
@@ -0,0 +1,33 @@
+text = "PYTHONPROGRAMMING"
+
+print("Original String :", text)
+
+# 1. First 6 characters
+print("1.", text[:6])
+
+# 2. Characters from index 6 to end
+print("2.", text[6:])
+
+# 3. Characters from index 2 to 8
+print("3.", text[2:9])
+
+# 4. Every second character
+print("4.", text[::2])
+
+# 5. Every third character
+print("5.", text[::3])
+
+# 6. Reverse the string
+print("6.", text[::-1])
+
+# 7. Last 5 characters
+print("7.", text[-5:])
+
+# 8. String without first character
+print("8.", text[1:])
+
+# 9. String without last character
+print("9.", text[:-1])
+
+# 10. Reverse every second character
+print("10.", text[::-2])
