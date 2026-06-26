c7907c3b5fa971a201117dfc5d448c8eb43410e5
sravanthiyaragani <sravanthiyara41@gmail.com>
2026-06-26 20:34:16 +0530

    python task 9

---
+++ assignment9.py
@@ -0,0 +1,29 @@
+# List Operations
+
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+
+# Sum of List
+total = 0
+for i in numbers:
+    total += i
+print("Sum of List =", total)
+
+# Product of List
+product = 1
+for i in numbers:
+    product *= i
+print("Product of List =", product)
+
+# Maximum Element
+maximum = max(numbers)
+print("Maximum =", maximum)
+
+# Minimum Element
+minimum = min(numbers)
+print("Minimum =", minimum)
+
+# Length of List
+length = len(numbers)
+print("Length =", length)
